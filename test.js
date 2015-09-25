88a98205931f9e80a2439e08a7f2d8b3ad72ad2a
Mike <lvlichaelly@gmail.com>
2015-09-25 16:08:51 -0700

    initial test and organize basic structures

---
+++ test.js
@@ -0,0 +1,15 @@
+console.log('this is a test');
+// 
+// backend
+//
+// Node.js (why? cuz it is fast, thanks to google's v8)
+// Faye (or could've used socket.io)
+//
+// front end
+// html
+// CSS
+// POJ
+// Faye
+//
+// Deployment heroku
+//
