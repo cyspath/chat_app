3bbbbdb5f6e5096be72ea3654c6688b74b8ead36
Mike <lvlichaelly@gmail.com>
2015-09-25 17:45:38 -0700

    working node basic chat app

---
+++ main.js
@@ -0,0 +1,40 @@
+// var http = require('http'), // createServer to specify callback func when any kind of http request is sent to our server
+//     fs = require('fs'); // this is used for reading contents of client.html form filesystem
+//
+// var app = http.createServer(function (request, response) {
+//   fs.readFile("client.html", 'utf-8', function (error, data) {
+//     response.writeHead(200, { 'Content-Type': 'text/html' });
+//     response.write(data);
+//     response.end();
+//   });
+//
+// }).listen(1337) // 2428 = chat =)
+//
+//
+// var io = require('socket.io').listen(app);
+//
+// io.sockets.on('connection', function(socket) { // will fire everytime a client socket connects to the servers socket.io module
+//   socket.on('message_to_server', function(data) {
+//     io.sockets.emit("message_to_client", { message: data["message"]});
+//   })
+// })
+
+
+var http = require('http'),
+    fs = require('fs');
+
+var app = http.createServer(function (request, response) {
+    fs.readFile("client.html", 'utf-8', function (error, data) {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+    });
+}).listen(2428);
+
+var io = require('socket.io').listen(app);
+
+io.sockets.on('connection', function(socket) {
+    socket.on('message_to_server', function(data) {
+        io.sockets.emit("message_to_client",{ message: data["message"] });
+    });
+});
