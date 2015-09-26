7debd9dfd75f74f814bf0cb5f22dbffbdc7eb3dd
Mike <lvlichaelly@gmail.com>
2015-09-25 17:20:35 -0700

    main.js finished for now, starting html

--- app/main.js
+++ app/main.js
@@ -12,3 +12,9 @@ var app = http.createServer(function (request, response) {
 
 
 var io = require('socket.io').listen(app);
+
+io.sockets.on('connection', function(socket) { // will fire everytime a client socket connects to the servers socket.io module
+  socket.on('message_to_server', function(data) {
+    io.sockets.emit("message_to_client", { message: data["message"]});
+  })
+})
